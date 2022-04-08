34cd2e6fe87236314323ed746581e57c28618f87
Tulio Ribeiro Maia <tulio.ribeiromaia@gmail.com>
2022-04-08 16:18:44 -0300

    implements ms history

---
+++ ms-history/src/test/java/shop/style/history/MsHistoryApplicationTests.java
@@ -0,0 +1,13 @@
+package shop.style.history;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsHistoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
