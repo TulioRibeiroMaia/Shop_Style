bd3dc113b86eb879e6c70934f08408ab5bbc7077
Tulio Ribeiro Maia <tulio.ribeiromaia@gmail.com>
2022-04-11 19:00:28 -0300

    implements exception ErrorMessage in ms history.

---
+++ ms-history/src/main/java/shop/style/history/Exception/ErrorMessage.java
@@ -0,0 +1,19 @@
+package shop.style.history.Exception;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import java.util.Date;
+
+@Getter
+@AllArgsConstructor
+public class ErrorMessage {
+
+    private int statusCode;
+
+    private Date timestamp;
+
+    private String message;
+
+    private String description;
+}
